8a166f557bdf321edba527f6c639296bb02afcd4
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-04-04 20:42:07 +0530

    new

---
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.example.Inventory_management_backend.controller;
+
+public class TestController {
+}
---
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/repository/TestRepository.java
@@ -0,0 +1,4 @@
+package com.example.Inventory_management_backend.repository;
+
+public interface TestRepository {
+}
