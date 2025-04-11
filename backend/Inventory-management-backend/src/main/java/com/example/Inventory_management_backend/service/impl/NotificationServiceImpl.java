7ce6ebbecba3a634342a1215e536039908e03dcb
kavishka-wilagedara <kavishkamadumal890@gmail.com>
2025-04-12 00:33:45 +0530

    implement product expiry notifications for upcoming and expired products

--- backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/service/impl/NotificationServiceImpl.java
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/service/impl/NotificationServiceImpl.java
@@ -4,10 +4,10 @@ import com.example.Inventory_management_backend.model.Product;
 import com.example.Inventory_management_backend.repository.ProductRepository;
 import com.example.Inventory_management_backend.service.NotificationService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -23,6 +23,28 @@ public class NotificationServiceImpl implements NotificationService {
         List<Product> products = productRepository.findAll();
         LocalDate today = LocalDate.now();
 
+        return  products.stream()
+                .filter(product -> product.getExpiryDate() != null)
+                .filter(product -> {
+                    long daysLeft = ChronoUnit.DAYS.between(today, product.getExpiryDate());
+                    return daysLeft <= 7;
+                })
+                .map(product -> {
+                    long daysLeft = ChronoUnit.DAYS.between(today, product.getExpiryDate());
 
+                    if(daysLeft > 0){
+                        return product.getProductName() + "(product id: "+product.getId()+") will expire in " + daysLeft + " days";
+                    }
+                    else if(daysLeft == 0){
+                        return product.getProductName() + "(product id: "+product.getId()+") will expire today";
+                    }
+                    else if(daysLeft == -1){
+                        return product.getProductName() + "(product id: "+product.getId()+") expired yesterday";
+                    }
+                    else{
+                        return product.getProductName() + "(product id: "+product.getId()+") expired before " + Math.abs(daysLeft) + " days";
+                    }
+                })
+                .toList();
     }
 }
