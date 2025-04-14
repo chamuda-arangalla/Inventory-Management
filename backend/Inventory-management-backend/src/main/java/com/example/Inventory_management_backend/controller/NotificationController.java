16747c6b35ffe1b70a6562026183b19cabbfa740
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-04-14 09:04:02 +0530

    new

--- backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/NotificationController.java
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.example.Inventory_management_backend.controller;
 import com.example.Inventory_management_backend.service.NotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/notifications")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class NotificationController {
 
     private NotificationService notificationService;
