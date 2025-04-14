044083a005a992e4f65e19fee374fa3dc3d2e857
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-04-14 06:39:10 +0530

    new

--- backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/FeedbackController.java
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/FeedbackController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/feedback")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class FeedbackController {
 
     private final FeedbackService feedbackService;
--- backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/UserController.java
+++ backend/Inventory-management-backend/src/main/java/com/example/Inventory_management_backend/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/user")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
